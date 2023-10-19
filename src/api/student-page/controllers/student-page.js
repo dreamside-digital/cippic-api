ad5d6cf8eece7eadcaa014dce15d158e5b3454fd
Sharon <sharon.peishan.kennedy@gmail.com>
2023-10-19 11:07:29 -0400

    add student page collection

---
+++ src/api/student-page/controllers/student-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * student-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::student-page.student-page');
---
+++ src/api/student-page/routes/student-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * student-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::student-page.student-page');
