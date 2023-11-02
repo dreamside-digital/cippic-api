1a088ed4d63ea90012768ea86310ea0bd6c9c778
Sharon <sharon.peishan.kennedy@gmail.com>
2023-11-02 10:32:38 -0400

    remove student page content type

--- src/api/student-page/controllers/student-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * student-page controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::student-page.student-page');
--- src/api/student-page/routes/student-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * student-page router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::student-page.student-page');
