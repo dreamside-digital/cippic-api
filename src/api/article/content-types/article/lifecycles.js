1a00e9f62f2562f6be8f3c346416c24885f1408e
Sharon <sharon.peishan.kennedy@gmail.com>
2024-05-01 23:18:45 -0400

    update afterUpdate lifecycle hook to use manually set date_published if present

--- src/api/article/content-types/article/lifecycles.js
+++ src/api/article/content-types/article/lifecycles.js
@@ -12,9 +12,20 @@ module.exports = {
     // run after article is published
     if (event.params.data.publishedAt) {
       const id = event.params.where.id
+      const article = await strapi.entityService.findOne("api::article.article", id)
+      
+      // default is to use the automated publishedAt date
+      let datePublished = event.params.data.publishedAt
 
-      // if date_published is manually set, use that date; otherwise use the default publishedAt
-      const datePublished = event.params.data.date_published ? event.params.data.date_published : event.params.data.publishedAt
+      // if the date_published was already manually set, keep using that
+      if (article.date_published) {
+        datePublished = article.date_published
+      }
+
+      // if the date_published is being updated, use the new date
+      if (event.params.data.date_published) {
+        datePublished = event.params.data.date_published
+      }
 
       // insert directly into database to avoid entering an infinite loop
       const update = await strapi.db
